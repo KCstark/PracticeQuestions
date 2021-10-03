6b9aa230a8da41a8af59f7eb208a31c9af8eefd4
KCstark <kanishakc1@gmail.com>
2021-10-03 18:40:19 +0530

    added lexicographical test for alphabet

---
+++ Reccursion/lexicography.java
@@ -0,0 +1,24 @@
+package Reccursion;
+
+public class Lexicography {
+
+    public static Boolean lexicoTest(String str){
+        int mark=0;
+        for(int i=0;i<str.length()-1;i++){
+            if(str.charAt(i)<=str.charAt(i+1)){
+                mark=1;
+            }else{
+                break;
+            }
+        }
+        if(mark==1)
+            return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int mark=0;
+        String str="aaaabc";
+        System.out.println(lexicoTest(str));
+    }
+}
